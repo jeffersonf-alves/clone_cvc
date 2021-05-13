ff3be5c8e34f986d79818cd5365ac320ed4e237b
jefferso alves <jefff9910@outlook.com>
2021-05-13 10:46:58 -0300

    version desktop

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var subMenu = document.getElementById('submenu');
 var trianguloChange = document.getElementById('changeTria');
-
+let verify = true;
 function showSubmenu() {
-    subMenu.style.display = "flex";
-    trianguloChange.classList.remove('triangulo');
-    trianguloChange.classList.add('trianguloChange');
-}
-function closeMenu() {
-    subMenu.style.display = "none";
-    trianguloChange.classList.remove('trianguloChange');
-    trianguloChange.classList.add('triangulo');
+ 
+    if(verify == true) {
+        subMenu.style.display = "flex";
+        trianguloChange.classList.remove('triangulo');
+        trianguloChange.classList.add('trianguloChange');
+        verify = false;
+    } 
+    else if(verify == false){
+        subMenu.style.display = "none";
+        trianguloChange.classList.remove('trianguloChange');
+        trianguloChange.classList.add('triangulo');
+        verify = true;
+    }
+    console.log(verify);
 }
